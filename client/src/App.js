054a09913a05ef9dccd0e4ddb85d69cbf6ae4a06
Dave <8709533+DaveBathnes@users.noreply.github.com>
2021-12-12 07:36:43 +0000

    Cleanup of front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,86 @@
-import './App.css';
+import './App.css'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardHeader from '@mui/material/CardHeader'
+import Grid from '@mui/material/Grid'
+import TextField from '@mui/material/TextField'
 
-import * as shortenerHelper from './helpers/shortener';
+import CssBaseline from '@mui/material/CssBaseline'
 
-function App() {
+import * as shortenerHelper from './helpers/shortener'
 
-  const [shortText, setShortText] = useState("");
-  const [longUrl, setLongUrl] = useState("");
-  const [result, setResult] = useState("");
+function App () {
+  const [alias, setAlias] = useState('')
+  const [url, setUrl] = useState('')
+  const [urlErrorText, setUrlErrorText] = useState('')
+  const [resultMessage, setResultMessage] = useState('')
 
-  const refreshShortText = () => {
-    const shortText = shortenerHelper.generateShortText();
-    return shortText;
+  const refreshAlias = () => {
+    setResultMessage('')
+    return shortenerHelper.generateAlias()
   }
 
   const makeShortUrl = () => {
-    const result = shortenerHelper.makeShortUrl(shortText, longUrl);
-    setResult(result);
+    setResultMessage('')
+    setUrlErrorText('')
+    setUrl('')
+    const result = shortenerHelper.makeShortUrl(alias, url)
+    if (result) {
+      setResultMessage('Created ' + window.location + alias)
+    } else {
+      refreshAlias()
+      setResultMessage('Error creating the short url')
+    }
+  }
+
+  const handleUrlChange = (event) => {
+    setUrlErrorText('')
+    setUrl(event.target.value)
+    try {
+      new URL(event.target.value) // eslint-disable-line no-new
+    } catch (e) {
+      setUrlErrorText('Must be a valid URL')
+      console.log(e)
+    }
   }
 
   useEffect(() => {
-    setShortText(refreshShortText());
-  }, []);
+    setAlias(refreshAlias())
+  }, [])
 
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justify="center"
-      style={{ minHeight: '100vh', padding: '10px' }}
-    >
-      <Grid item xs={3}>
-        <Card elevation={0} variant="outlined">
-          <CardHeader title="Short Links" />
-          <CardContent>
-            <p>Enter your long URL</p>
-            <TextField id="txt-long" label="Long URL" variant="outlined" onChange={(e) => setLongUrl(e.value)} />
-            <p>{window.location.hostname + '/' + shortText}</p>
-          </CardContent>
-          <CardContent>
-            <p>{result}</p>
-          </CardContent>
-          <CardActions>
-            <Button size="small" onClick={() => makeShortUrl()}>Make short URL</Button>
-          </CardActions>
-        </Card>
+    <>
+      <CssBaseline />
+      <Grid
+        container
+        spacing={0}
+        direction='column'
+        alignItems='center'
+        justify='center'
+        style={{ minHeight: '100vh', padding: '10px' }}
+      >
+        <Grid item xs={3}>
+          <Card elevation={0} variant='outlined'>
+            <CardHeader title='Short URLs' />
+            <CardContent>
+              <p>Enter your URL</p>
+              <TextField value={url} error={urlErrorText !== ''} helperText={urlErrorText} id='txt-url' label='URL' variant='outlined' onChange={handleUrlChange} />
+              <p>Proposed alias <strong>{alias}</strong></p>
+              <p>{resultMessage}</p>
+            </CardContent>
+            <CardActions>
+              <Button size='small' onClick={() => makeShortUrl()} disabled={url === '' || urlErrorText !== ''}>Make short URL</Button>
+            </CardActions>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
--- client/src/helpers/shortener.js
+++ client/src/helpers/shortener.js
@@ -3,18 +3,18 @@ import axios from 'axios'
 const config = require('./config.json')
 
 export const makeShortUrl = async (short, long) => {
-    const result = await axios.post(`${config.apiUrl}`, { short: short, long: long });
-    return result;
+  const result = await axios.post(`${config.apiUrl}`, { short: short, long: long })
+  return result
 }
 
-export const generateShortText = () => {
-    const characters = "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ";
-    let shortText = "";
+export const generateAlias = () => {
+  const characters = '123456789ABCDEFGHIJKLMNPQRSTUVWXYZ'
+  let alias = ''
 
-    for (let i = 0; i < 6; i++) {
-        const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length));
-        shortText += randomCharacter;
-    }
+  for (let i = 0; i < 5; i++) {
+    const randomCharacter = characters.charAt(Math.floor(Math.random() * characters.length))
+    alias += randomCharacter
+  }
 
-    return shortText;
+  return alias
 }
