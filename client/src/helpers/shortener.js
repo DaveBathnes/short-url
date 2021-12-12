82f6a2aa01f4c16573bc3e40e42c88883cf12e16
Dave <8709533+DaveBathnes@users.noreply.github.com>
2021-12-12 23:06:07 +0000

    Some error handling for 500s

--- client/src/helpers/shortener.js
+++ client/src/helpers/shortener.js
@@ -3,8 +3,12 @@ import axios from 'axios'
 const config = require('./config.json')
 
 export const makeShortUrl = async (alias, url) => {
-  const result = await axios.post(`/${config.apiUrl}${alias}`, { url: url })
-  return (result.status === 200)
+  try {
+    const result = await axios.post(`/${config.apiUrl}${alias}`, { url: url })
+    return (result.status === 200)
+  } catch (e) {
+    return false;
+  }
 }
 
 export const generateAlias = () => {
